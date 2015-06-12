3f71974a2d853a3af67f70bf113a70214afa6fea
Srikanth Thiagarajan <sriks2008@gmail.com>
2015-06-12 00:27:54 -0700

    minor changes

--- src/main/java/com/expedia/web/Server.java
+++ src/main/java/com/expedia/web/Server.java
@@ -4,24 +4,15 @@ import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
 
 public class Server {
-    private HttpServer server;
 
-    public void start() throws IOException {
+    public static void main(String [] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
         server.createContext("/test", new RestEndpointHandler());
         server.createContext("/delay", new DelayedResponseHandler(10));
-        server.setExecutor(null);
-        server.start();
-    }
-
-    public void stop() {
-        server.stop(0);
-    }
-
-    public static void main(String [] args) throws Exception {
-        Server server = new Server();
+        server.setExecutor(Executors.newFixedThreadPool(10));
         server.start();
     }
 }
