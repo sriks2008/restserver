f38490fe01f4cf76d961ba30c4ff618b386be7e3
Srikanth Thiagarajan <sriks2008@gmail.com>
2015-06-16 16:31:56 -0700

    changing server port

--- src/main/java/com/expedia/web/Server.java
+++ src/main/java/com/expedia/web/Server.java
@@ -9,9 +9,10 @@ import java.util.concurrent.Executors;
 public class Server {
 
     public static void main(String [] args) throws Exception {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-        server.createContext("/test", new RestEndpointHandler());
-        server.createContext("/delay", new DelayedResponseHandler(10));
+        HttpServer server = HttpServer.create(new InetSocketAddress(9000), 0);
+        server.createContext("/test", new RestEndpointHandler()).getFilters().add(new ParameterFilter());
+        server.createContext("/test/summary", new RestEndpointSummaryHandler()).getFilters().add(new ParameterFilter());
+        server.createContext("/delay", new DelayedResponseHandler(10)).getFilters().add(new ParameterFilter());
         server.setExecutor(Executors.newFixedThreadPool(10));
         server.start();
     }
